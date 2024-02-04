a1f7b48c5481f7c3965db1ea0658c62639f81a95
Jesse Higgins <higgins.je@northeastern.edu>
2024-02-04 11:39:59 -0500

    count practice 04

---
+++ 04Practice/count_up.py
@@ -0,0 +1,17 @@
+
+
+def count_up(number, count = 0):
+
+    while count < number:
+
+        count = count + 1
+
+        print(count)
+
+
+def main():
+
+    count_up(10)
+
+if __name__ == "__main__":
+    main()
