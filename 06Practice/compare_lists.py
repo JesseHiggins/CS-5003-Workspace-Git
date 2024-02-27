4cfa35516fa3b6ed68c56e2362378b07360150ad
Jesse Higgins <johndoe@email.comhiggins.je@northeastern.edu>
2024-02-27 16:36:27 -0500

    commit

--- 06Practice/compare_lists.py
+++ 06Practice/compare_lists.py
@@ -1,15 +1,20 @@
 def compare_lists(list1, list2):
-
-    for i in range(0, len(list1)):
+    list = []
+    for i in list1:
         for j in list2:
             if i == j:
-                count += 1
-    return evenlst
+                list += [i]
+    if list1 == list:
+        return True
+    else:
+        return False
 
 def main():
 
-    list = [1, 2, -3, 4, -5]
-    print(filter_evens(list))
+    list1 = [1, 2, 3, 12]
+    list2 = [3, 2, 1, 5, 7]
+
+    print(compare_lists(list1, list2))
 
 if __name__ == "__main__":
     main()
--- 06Practice/positional_elements.py
+++ 06Practice/positional_elements.py
@@ -1,6 +1,7 @@
 def positional_elements(list1):
     count = 0
-    for i in list1:
+    for i in range(len(list1)):
+        print(list1[i])
         if list1[i] == i:
             count += 1
 
@@ -8,7 +9,7 @@ def positional_elements(list1):
 
 def main():
 
-    list = [1, 2, 3]
+    list = [5, 4, 3, 2, 1, -1]
     print(positional_elements(list))
 
 if __name__ == "__main__":
