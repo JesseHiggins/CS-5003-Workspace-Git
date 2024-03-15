8e3a4e1247e03cdf69b5fb8ec5a334eeb980988f
Jesse Higgins <higgins.je@northeastern.edu>
2024-03-15 15:46:40 -0400

    gg

--- 07/07 Practice/find_value.py
+++ 07/07 Practice/find_value.py
@@ -1,18 +1,21 @@
 def sum_values(list, index, string):
     print("Start", index, list[index])
     # if index < len(list) - 1:
-    if list[index] == string:
-            return string
+    if index < 0:
+        return "no"
+    elif list[index] == string:
+            return "yes"
     else:
-        sum_values(list, index - 1, string)
-    print("End", index)
+        return sum_values(list, index - 1, string)
     
     
 
 def main():
 
     list = ["g", "t", "hi", "hello"]
-    print(sum_values(list, 0, "hi"))
+    index = len(list) - 1
+
+    print(sum_values(list, index, "t"))
           
 if __name__ == "__main__":
     main()
---
+++ 07/07 Practice/in_english.py
@@ -0,0 +1,14 @@
+def in_english(num):
+
+    pass
+
+def main():
+
+    num = 5
+    num = num.split()
+
+    
+    print(num[1])
+
+if __name__ == "__main__":
+    main()
